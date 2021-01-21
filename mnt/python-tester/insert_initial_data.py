f5386f71e3c9c8820d63c976ec1fc58dbd08afa8
Marcos Marx <marcosmarxm@users.noreply.github.com>
2021-01-20 21:02:54 -0300

    correct test and dag (#5)

--- mnt/python-tester/insert_initial_data.py
+++ mnt/python-tester/insert_initial_data.py
@@ -1,8 +1,14 @@
+import os
 import pandas as pd
 from sqlalchemy import create_engine
 
-source = create_engine('postgresql+psycopg2://root:root@source-db:5432/source')
-dest = create_engine('postgresql+psycopg2://root:root@dest-db:5432/dest')
 
-pd.read_csv('./data/source_sample.csv').to_sql(name='transactions', con=source, if_exists='replace', index=False)
-pd.read_csv('./data/source_sample.csv').head(1).to_sql(name='transactions', con=dest, if_exists='replace', index=False)
+def insert_initial_data(tablename):
+    source = create_engine('postgresql+psycopg2://root:root@source-db:5432/source')
+    dest = create_engine('postgresql+psycopg2://root:root@dest-db:5432/dest')
+    sample_data = pd.read_csv(f'./data/sample_{tablename}.csv')
+    sample_data.to_sql(name=tablename, con=source, if_exists='replace', index=False)
+    sample_data.head(1).to_sql(name=tablename, con=dest, if_exists='replace', index=False)
+
+
+insert_initial_data('transactions')
