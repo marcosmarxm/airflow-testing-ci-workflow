6fd4f3007ea5862694ad0f6066a1d083b19977c9
Marcos Marx <marcosmarxm@users.noreply.github.com>
2021-01-29 16:47:20 -0300

    refactor dag and improve tests (#17)

--- dags/dag_sales_pipeline.py
+++ dags/dag_sales_pipeline.py
@@ -1,25 +1,47 @@
-from airflow.decorators import dag, task
+from airflow import DAG
 from airflow.utils.dates import days_ago
 from airflow.providers.postgres.hooks.postgres import PostgresHook
 from airflow.providers.postgres.operators.postgres import PostgresOperator
+from airflow.operators.python import PythonOperator
 
 
-@dag(default_args={'owner': 'airflow'},
-     schedule_interval=None,
-     start_date=days_ago(2),
-     template_searchpath='/opt/airflow/sql/sales/',
-     tags=['etl', 'analytics', 'sales'])
-def products_sales_pipeline():
+def transfer_oltp_olap(**kwargs):
+    """Get records from OLTP and transfer to OLAP database"""
+    dest_table = kwargs.get('dest_table')
+    sql = kwargs.get('sql')
+    params = kwargs.get('params')
+
+    oltp_hook = PostgresHook(postgres_conn_id='oltp')
+    olap_hook = PostgresHook(postgres_conn_id='olap')
+    data_extracted = oltp_hook.get_records(sql=sql, parameters=params)
+    olap_hook.insert_rows(dest_table, data_extracted, commit_every=1000)
+
+
+with DAG(dag_id='products_sales_pipeline',
+         default_args={'owner': 'airflow'},
+         schedule_interval=None,
+         start_date=days_ago(2),
+         template_searchpath='/opt/airflow/sql/sales/',
+         tags=['etl', 'analytics', 'sales']) as dag:
 
     execution_date = '{{ ds }}'
 
-    @task()
-    def transfer_oltp_olap(dest_table, sql, params=None):
-        """Get records from OLTP and transfer to OLAP database"""
-        oltp_hook = PostgresHook(postgres_conn_id='oltp')
-        olap_hook = PostgresHook(postgres_conn_id='olap')
-        data_extracted = oltp_hook.get_records(sql=sql, parameters=params)
-        olap_hook.insert_rows(dest_table, data_extracted, commit_every=1000)
+    load_incremental_transactions_data = PythonOperator(
+        task_id='load_incremental_transactions',
+        python_callable=transfer_oltp_olap,
+        op_kwargs={
+            'dest_table': 'stg_transactions',
+            'sql': 'select * from transactions where "purchase_date" = %s',
+            'params': [execution_date],
+        })
+
+    load_full_products_data = PythonOperator(
+        task_id='load_full_products',
+        python_callable=transfer_oltp_olap,
+        op_kwargs={
+            'dest_table': 'stg_products',
+            'sql': 'select * from products',
+        })
 
     delete_products_sales_exec_date = PostgresOperator(
         task_id='delete_products_sales_exec_date',
@@ -45,19 +67,6 @@ def products_sales_pipeline():
         sql='agg_sales_category.sql'
     )
 
-
-    load_incremental_transactions_data = transfer_oltp_olap(
-        dest_table='stg_transactions',
-        sql='select * from transactions where "purchase_date" = %s',
-        params=[execution_date]
-    )
-    load_full_products_data = transfer_oltp_olap(
-        dest_table='stg_products',
-        sql='select * from products'
-    )
     [load_full_products_data, load_incremental_transactions_data, delete_products_sales_exec_date] >> join_transactions_products
     join_transactions_products >> union_incremental_products_sales
     union_incremental_products_sales >> agg_sales_category
-
-
-dag_prod_sales_pipeline = products_sales_pipeline()
